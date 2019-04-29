f9d16888192ac22c86a50e2b3a7aa3e750ecae49
Aram Gumrikyan <aram.gumrikyan.@tumo.org>
2019-04-29 21:18:28 +0400

    add files

---
+++ class.deadanimal.js
@@ -0,0 +1,24 @@
+class Deadanimal {
+   constructor(x, y, index) {
+       this.x = x;
+       this.y = y;
+       this.energy = 0;
+       this.index = index;
+   }
+   erosion() {
+       this.energy++;
+       if (this.energy >= 30) {
+           this.die();
+           return;
+       }
+   }
+   die() {
+       for (var i in deadanimalsArr) {
+           if (this.x == deadanimalsArr[i].x && this.y == deadanimalsArr[i].y) {
+               deadanimalsArr.splice(i, 1);
+               break;
+           }
+       }
+       matrix[this.y][this.x] = 0;
+   }
+}
